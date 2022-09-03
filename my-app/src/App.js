44149556a4ddf8f10726905a22406b696805a99e
Marielle Lunnan <marielle.lunnan@learnet.se>
2022-09-03 12:51:20 +0200

    Title

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,15 @@
-import Sidenav from "./sidenav";
+import Sidenav from "./sidenav/sidenav";
+import Title from "./title/title";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Title />
         <Sidenav />
       </header>
     </div>
   );
 }
-
 export default App;
---
+++ my-app/src/title/title.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./title.css"
+
+function Title (){
+    return (
+        <div>
+            <h1 className="Title">This is my REACT page!</h1>
+        </div>
+    )
+}
+
+export default Title
