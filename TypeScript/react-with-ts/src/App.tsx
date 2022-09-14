d5fcee2f1230818d0f397faeaff357ec5117af22
Marielle Lunnan <marielle.lunnan@learnet.se>
2022-09-14 10:58:09 +0200

    products

--- TypeScript/react-with-ts/src/App.tsx
+++ TypeScript/react-with-ts/src/App.tsx
@@ -1,24 +1,36 @@
-import React from "react";
-import "./App.css";
-import Product from "./components/Product";
-import data from "./data.json";
+import React, { useState } from 'react'
+import './App.css'
+import { Product } from './components/export'
+import data from './data.json'
 
 function App() {
-  return (
-    <div className="App">
-      <h2>My React ts App</h2>
-      <div style={{ display: "flex", flexDirection: "row" }}>
-        {data.map((item) => (
-          <Product
-            imgSrc={item.imgSrc}
-            name={item.name}
-            description={item.description}
-          />
-        ))}
-      </div>
-      {/*<Product />*/}
-    </div>
-  );
+    const [itemsBought, setItemsBought] = useState(0)
+
+    const onBuyItem = (clicks: number) => {
+        const totalItemsBought = itemsBought + clicks
+        setItemsBought(totalItemsBought)
+    }
+    return (
+        <div className="App">
+            <h2>My React ts App</h2>
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
+                {data.map((item) => (
+                    <Product
+                        imgSrc={item.imgSrc}
+                        name={item.name}
+                        description={item.description}
+                    />
+                ))}
+                <Product
+                    imgSrc={data[0].imgSrc}
+                    name={data[0].name}
+                    description={data[0].description}
+                    buttonText={'buy'}
+                />
+            </div>
+            <div>Items bought: {itemsBought}</div>
+        </div>
+    )
 }
 
-export default App;
+export default App
--- TypeScript/react-with-ts/src/components/Product.tsx
+++ TypeScript/react-with-ts/src/components/Product.tsx
@@ -1,20 +1,31 @@
-import React from "react";
-import "./styles.css";
+import React from 'react'
+import './styles.css'
 
 interface IProduct {
-  imgSrc: string;
-  name: string;
-  description: string[];
+    imgSrc: string
+    name: string
+    description: string[]
+    buttonText?: string | number
+    functionToRun?: () => void
 }
 
 const Product = (props: IProduct) => {
-  return (
-    <div className="wrapper">
-      <img src={props.imgSrc} className="product-image" />
-      <h3>{props.name}</h3>
-      <p>{props.description[0]}</p>
-      <p>{props.description[1]}</p>
-    </div>
-  );
-};
-export default Product;
+    const onButtonClick = () => {
+        if (props.functionToRun !== undefined) {
+            props.functionToRun()
+        }
+    }
+    return (
+        <div className="wrapper">
+            <img src={props.imgSrc} className="product-image" />
+            <h3>{props.name}</h3>
+            <p>{props.description[0]}</p>
+            <p>{props.description[1]}</p>
+
+            {props.buttonText !== undefined && (
+                <button onClick={onButtonClick}>{props.buttonText}</button>
+            )}
+        </div>
+    )
+}
+export default Product
